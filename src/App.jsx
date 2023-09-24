01077f01476a260cc20d1e366986cbb99352e95d
Abdul Kader <mak.sabuj@hotmail.com>
2023-09-24 22:18:59 +0600

    form submit

--- src/App.jsx
+++ src/App.jsx
@@ -45,88 +45,101 @@ const App = () => {
     const demo1 = () =>{
         alert('Hello Arrow Function')
     }
+    
+    const postFromData = (event) =>{
+        // Disabled default setting of form
+        event.preventDefault(); 
+        alert('Form Submitted');
+    }
 
     return (
         <>
-            <Fragment>
-                <Header/>
-                <Hero/>
-                <ContactForm/>
-                <Footer/>
-            </Fragment>
-
-            <div>
-                <h1 style={{color: 'red'}}>Heading</h1>
-                <button onClick={() => alert("Hi, Good Afternoon!")}>Submit</button>
-
-                {
-                    marks >= 80 ? <h1>Brilliant Result</h1> : <h1>Average Result</h1>
-                }
-                {/* immediately invoked function */}
-
-                {(
-                    () => {
-                        if (marks >= 80 && marks < 100) {
-                            return <h1>A+</h1>
-                        } else if (marks >= 70 && marks < 80) {
-                            return <h1>A</h1>
-                        } else if (marks >= 60 && marks < 70) {
-                            return <h1>B+</h1>
-                        } else {
-                            return <h1>F</h1>
-                        }
-                    }
-                )()}
-
-                {/* Loop inside implementation using map */}
-
-                {
-                    city.map((item, index) => {
-                        return <li key={index.toString()}>{item}</li>
-                    })
-                }
-
-            </div>
-
-            {/*    Conditional Rendering    */}
-            <h1>Login Status</h1>
-            {loginStatusBtn(true)}
-
-            {/* Ternary or If else */}
-
-            {
-                status ?
-                    <button>Logout Button</button> :
-                    <button>Login Button</button>
-            }
-
-            {/*    Logical && and ||    */}
-
-            <br/>
-            <h1>Login Status</h1>
-            {
-                status && <button>Logout Button1</button>
-            }
-
-            {/*    Immediately invoked function for conditional rendering */}
-
-            {(() => {
-                if (status) {
-                    return <button>Logout Button2</button>
-                } else {
-                    return <button>Login Button2</button>
-                }
-            })()}
-
-            <Hero childBtnClick={btnClick} {...ItemObj}/>
-            {/*<ul>*/}
-            {/*    <li>Name: {ItemObj['name']}</li>*/}
-            {/*    <li>Age: {ItemObj.age}</li>*/}
-            {/*    <li>City: {ItemObj.city}</li>*/}
-            {/*</ul>*/}
+            {/*<Fragment>*/}
+            {/*    <Header/>*/}
+            {/*    <Hero/>*/}
+            {/*    <ContactForm/>*/}
+            {/*    <Footer/>*/}
+            {/*</Fragment>*/}
+            
+            {/*<div>*/}
+            {/*    <h1 style={{color: 'red'}}>Heading</h1>*/}
+            {/*    <button onClick={() => alert("Hi, Good Afternoon!")}>Submit</button>*/}
+            
+            {/*    {*/}
+            {/*        marks >= 80 ? <h1>Brilliant Result</h1> : <h1>Average Result</h1>*/}
+            {/*    }*/}
+            {/*    /!* immediately invoked function *!/*/}
+            
+            {/*    {(*/}
+            {/*        () => {*/}
+            {/*            if (marks >= 80 && marks < 100) {*/}
+            {/*                return <h1>A+</h1>*/}
+            {/*            } else if (marks >= 70 && marks < 80) {*/}
+            {/*                return <h1>A</h1>*/}
+            {/*            } else if (marks >= 60 && marks < 70) {*/}
+            {/*                return <h1>B+</h1>*/}
+            {/*            } else {*/}
+            {/*                return <h1>F</h1>*/}
+            {/*            }*/}
+            {/*        }*/}
+            {/*    )()}*/}
+            
+            {/*    /!* Loop inside implementation using map *!/*/}
+            
+            {/*    {*/}
+            {/*        city.map((item, index) => {*/}
+            {/*            return <li key={index.toString()}>{item}</li>*/}
+            {/*        })*/}
+            {/*    }*/}
+            
+            {/*</div>*/}
+            
+            {/*/!*    Conditional Rendering    *!/*/}
+            {/*<h1>Login Status</h1>*/}
+            {/*{loginStatusBtn(true)}*/}
+            
+            {/*/!* Ternary or If else *!/*/}
+            
+            {/*{*/}
+            {/*    status ?*/}
+            {/*        <button>Logout Button</button> :*/}
+            {/*        <button>Login Button</button>*/}
+            {/*}*/}
+            
+            {/*/!*    Logical && and ||    *!/*/}
+            
+            {/*<br/>*/}
+            {/*<h1>Login Status</h1>*/}
+            {/*{*/}
+            {/*    status && <button>Logout Button1</button>*/}
+            {/*}*/}
+            
+            {/*/!*    Immediately invoked function for conditional rendering *!/*/}
+            
+            {/*{(() => {*/}
+            {/*    if (status) {*/}
+            {/*        return <button>Logout Button2</button>*/}
+            {/*    } else {*/}
+            {/*        return <button>Login Button2</button>*/}
+            {/*    }*/}
+            {/*})()}*/}
+            
+            {/*<Hero childBtnClick={btnClick} {...ItemObj}/>*/}
+            {/*/!*<ul>*!/*/}
+            {/*/!*    <li>Name: {ItemObj['name']}</li>*!/*/}
+            {/*/!*    <li>Age: {ItemObj.age}</li>*!/*/}
+            {/*/!*    <li>City: {ItemObj.city}</li>*!/*/}
+            {/*/!*</ul>*!/*/}
+            
+            {/*<div>*/}
+            {/*    <button onClick={demo1}>Submit</button>*/}
+            {/*</div>*/}
             
             <div>
-                <button onClick={demo1}>Submit</button>
+                <form onSubmit={postFromData}>
+                    <input placeholder='name'/>
+                    <button type="submit">Submit</button>
+                </form>
             </div>
 
         </>
